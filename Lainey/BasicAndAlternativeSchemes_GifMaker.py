39a43c785cc59c7a17e66e9486580afeccfdbd24
Lainey <126870308+LaineyLouise@users.noreply.github.com>
2023-11-17 01:46:19 +0000

    Add files via upload

---
+++ Lainey/BasicAndAlternativeSchemes_GifMaker.py
@@ -0,0 +1,60 @@
+# MTMW12 Introduction to Numerical Modelling
+# Assignment 4: Advection
+# Basic Schemes Gif Maker
+# Student number: 31827822
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, PillowWriter
+from BasicAndAlternativeSchemes_Final import Schemes
+
+def animate(nt, ax, x, nx, dx, dt, a, b, c, u, ICnumber):
+    ax.clear()
+    
+    analytic, FTCS = Schemes(x, nx, dx, nt, dt, a, b, c, u, ICnumber, SNumber='FTCS')
+    analytic, WB = Schemes(x, nx, dx, nt, dt, a, b, c, u, ICnumber, SNumber='WB')
+    analytic, LW = Schemes(x, nx, dx, nt, dt, a, b, c, u, ICnumber, SNumber='LW')
+    analytic, TVD = Schemes(x, nx, dx, nt, dt, a, b, c, u, ICnumber, SNumber='TVD')
+          
+    ana, = plt.plot(x, analytic, color='b', label='Analytic')
+    ftcs, = plt.plot(x, FTCS, color='r', label='FTCS')
+    wb, = plt.plot(x, WB, color='purple', label='WB')
+    lw, = plt.plot(x, LW, color='orange', label='LW')
+    tvd, = plt.plot(x, TVD, color='orange', label='TVD')
+    
+    plt.title(u"Advection for {} timesteps and I.C.={}".format(nt, ICnumber))
+    plt.xlabel('X')
+    plt.ylabel('$\Phi$')
+    plt.xlim([0,1])
+    plt.ylim([-0.4,1.5])
+    plt.legend(loc='upper right')
+    return ana, ftcs, wb, lw #,init, 
+
+def gifmaker(nt, ICnumber):
+    fig, ax = plt.subplots()
+    # The variables kept constant:
+    xmin = 0
+    xmax = 1
+    a = 0.1
+    b = 0.5
+    c = 0.4
+    nx = 101
+    u = 1 
+    dx = (xmax - xmin)/(nx-1) 
+    dt = c*dx/u
+    x = np.linspace(xmin, xmax, nx) 
+    
+    ani = FuncAnimation(fig, animate, blit=True, interval=40, repeat=True,
+                        frames=nt, fargs=(ax, x, nx, dx, dt, a, b, c, u, ICnumber,))    
+    ani.save("InitialCondition{}For{}TimestepsBasicAndAlternative.gif".format(ICnumber, nt),
+             dpi=300, writer=PillowWriter(fps=25))
+   
+# Make gifs for initial conditions 1 and 2. 
+gifmaker(nt=150, ICnumber=1)
+gifmaker(nt=150, ICnumber=2)
+
+
+
+
+
+
